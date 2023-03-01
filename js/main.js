8d063b9d10b15782bfccaa1110a78c6affb8023b
Ahmed Hisham <ahmedhishamkamal@icloud.com>
2023-03-01 17:21:28 +0200

    Dark Mood

--- js/main.js
+++ js/main.js
@@ -1,47 +1,57 @@
-let input = document.getElementById('taskdata');
-let addbtn = document.getElementById('addbtn');
-let notask = document.getElementById('notask');
-let tasks = document.getElementById('tasks');
-let model = document.getElementById('model');
-let show = document.getElementById('show');
-let closee = document.getElementById('close');
+let input = document.getElementById("taskdata");
+let addbtn = document.getElementById("addbtn");
+let notask = document.getElementById("notask");
+let tasks = document.getElementById("tasks");
+let model = document.getElementById("model");
+let show = document.getElementById("show");
+let closee = document.getElementById("close");
+let switchAppear = document.getElementById("checkbox");
 
 let showMoel = () => {
- model.classList.toggle('display')  ;
-}
-show.addEventListener('click' , showMoel);
-closee.addEventListener('click', showMoel)
+  model.classList.toggle("display");
+};
+show.addEventListener("click", showMoel);
+closee.addEventListener("click", showMoel);
 
 let showNoTasks = () => {
-    if (tasks.childElementCount == 0) {
-        notask.classList.remove('none');
-    }
-}
+  if (tasks.childElementCount == 0) {
+    notask.classList.remove("none");
+  }
+};
 let addTask = () => {
-    let task = input.value;
-    if (task.trim() == 0 || task.length < 2 || task.length > 20) {
-        alert("you Must enter Valid Data")
-    } else {
-        notask.classList.add('none');
-        tasks.innerHTML += `<div class='task alert alert-info'> ${task}
+  let task = input.value;
+  if (task.trim() == 0 || task.length < 2 || task.length > 20) {
+    alert("you Must enter Valid Data");
+  } else {
+    notask.classList.add("none");
+    tasks.innerHTML += `<div class='task alert alert-info'> ${task}
         <button  class='delete float-right btn btn-danger'> Delete </button>
         </div>`;
-        showMoel();
+    showMoel();
+  }
+  tasks.addEventListener("click", function (ts) {
+    if (ts.target.classList.contains("task")) {
+      ts.target.classList.toggle("checked");
     }
-    tasks.addEventListener('click', function (ts) {
-        if (ts.target.classList.contains('task')) {
-            ts.target.classList.toggle('checked');
-        }
-    })
-    input.value = "";
-}
-addbtn.addEventListener('click', addTask);
+  });
+  input.value = "";
+};
+addbtn.addEventListener("click", addTask);
 
 let deleteTask = (e) => {
-    if (e.target.classList.contains('delete')) {
-        e.target.parentElement.remove();
-        showNoTasks();
-    }
-}
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+    showNoTasks();
+  }
+};
 
-document.addEventListener('click', deleteTask)
+document.addEventListener("click", deleteTask);
+
+switchAppear.addEventListener("change", function (e) {
+  if (e.currentTarget.checked) {
+    document.body.style.backgroundColor = "#FFF";
+    // document.body.style.backgroundColor = "#FFF";
+  } else {
+    document.body.style.backgroundColor = "#111";
+  }
+});
